050706342330acf154c1a6122feb425dd9eaee39
Katherine928 <katherine71317@gmail.com>
2023-03-15 18:54:21 -0400

    adding basic UI/functions

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import "./styles/app.scss";
+import data from "./util";
+import Library from "./components/Library";
+
+function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
+      <Library songs={songs} setCurrentSong={setCurrentSong} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LibrarySong.js
@@ -0,0 +1,16 @@
+const LibrarySong = ({ song, songs, setCurrentSong, id }) => {
+  const songSelectHandler = () => {
+    const selectedSong = songs.filter((state) => state.id === id);
+    setCurrentSong(selectedSong[0]);
+  };
+  return (
+    <div onClick={songSelectHandler} className="library-song">
+      <img src={song.cover} alt={song.name} />
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+export default LibrarySong;
